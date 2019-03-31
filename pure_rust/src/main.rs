891f3d17f67a828d547d59a3c363e4fae00904c5
Alex Addy <alex.addy@gmail.com>
2019-03-30 21:31:12 -0600

    Change pure-rust over to using probability matrix
    Still pretty rough. Needs cleanup.

--- pure_rust/src/main.rs
+++ pure_rust/src/main.rs
@@ -6,8 +6,8 @@ const SIZE: usize = 128;
 
 fn main() {
     let mut img = ImageBuffer::new(SIZE as u32, SIZE as u32);
-    let canvas = random_blending::random_blending(SIZE, 10, 4);
-    println!("After call");
+    let canvas = random_blending::random_blending(SIZE, 50);
+    println!("Converting raw to image");
     for (x, row) in canvas.into_iter().enumerate() {
         for (y, c) in row.into_iter().enumerate() {
             let pixel = Luma([c]);
--- pure_rust/src/random_blending.rs
+++ pure_rust/src/random_blending.rs
@@ -1,7 +1,7 @@
-use std::cmp::min;
-
-use rand::prelude::*;
 use rand::distributions::Uniform;
+use rand::prelude::*;
+
+const PROBABILITIES: &[usize; 9] = &[1, 4, 5, 1, 0, 5, 1, 1, 1];
 
 /// Generate a square matrix filled with noise
 fn gen_matrix(size: usize) -> Vec<Vec<u8>> {
@@ -18,54 +18,88 @@ fn gen_matrix(size: usize) -> Vec<Vec<u8>> {
     mat
 }
 
-/// Calculate upper and lower bounds from index and distance.
-///
-/// Will not return a value lower than 0 or greater than max
-fn calc_bounds(idx: usize, dist: usize, max: usize) -> (usize, usize) {
-    let low = idx.saturating_sub(dist);
-    let high = min(idx+dist, max);
-    (low, high)
+fn pick_from_probability() -> usize {
+    let mut rng = thread_rng();
+    let total: usize = PROBABILITIES.iter().sum();
+    let pick = rng.gen_range(0, total);
+    let mut choice_idx = 0;
+    let mut sum = PROBABILITIES[choice_idx];
+    while sum < pick {
+        choice_idx += 1;
+        sum += PROBABILITIES[choice_idx];
+    }
+
+    choice_idx
+}
+
+fn convert_pick_to_rel_idx(pick: usize) -> (isize, isize) {
+    // TODO make this general
+    match pick {
+        0 => (-1, -1),
+        1 => (-1, 0),
+        2 => (-1, 1),
+        3 => (0, -1),
+        4 => (0, 0),
+        5 => (0, 1),
+        6 => (1, -1),
+        7 => (1, 0),
+        8 => (1, 1),
+        x => panic!("Got pick with impossible value of {}", x),
+    }
+}
+
+fn get_idx(i: usize, j: usize, size: usize) -> (usize, usize) {
+    let i = i as isize;
+    let j = j as isize;
+    let size = size as isize;
+
+    loop {
+        let (row_diff, col_diff) = convert_pick_to_rel_idx(pick_from_probability());
+        // ensure row_diff is in range
+        if row_diff + i >= size || row_diff + i < 0 {
+            continue;
+        }
+        // ensure col_diff is in range
+        if col_diff + j >= size || col_diff + j < 0 {
+            continue;
+        }
+        return ((row_diff + i) as usize, (col_diff + j) as usize);
+    }
 }
 
 /// Randomly blend pixels from placing results into into
 ///
 /// assumes from and into are the same size and are square
-fn blend_into(square_size: usize, from: &Vec<Vec<u8>>, into: &mut Vec<Vec<u8>>) {
-    let mut rng = thread_rng();
+fn blend_into(size: usize, from: &Vec<Vec<u8>>, into: &mut Vec<Vec<u8>>) {
     for i in 0..from.len() {
         for j in 0..from.len() {
-            //let blend_i = rng.gen_range(calc_bounds(i, square_size, from.len()));
-            //let blend_j = rng.gen_range(calc_bounds(j, square_size, from.len()));
-
-            let blend_i = rng.gen_range(i.saturating_sub(square_size), min(i+square_size, from.len()));
-            let blend_j = rng.gen_range(j.saturating_sub(square_size), min(j+square_size, from.len()));
+            let (blend_i, blend_j) = get_idx(i, j, size);
             into[i][j] = ((from[i][j] as u16 + from[blend_i][blend_j] as u16) / 2) as u8;
         }
     }
 }
 
-
 /// Generate a new grayscale image via random blending
-pub fn random_blending(size: usize, iterations: usize, square_size: usize) -> Vec<Vec<u8>> {
+pub fn random_blending(size: usize, iterations: usize) -> Vec<Vec<u8>> {
     let mut first = gen_matrix(size);
     let mut second = first.clone();
     if iterations == 0 {
         // Not sure why this would happen but handling it here makes later easier
         return first;
     }
-    if square_size == 0 || square_size == 1 {
+    if size == 0 || size == 1 {
         // If the square size is 0 or 1 no amount of iterations will change the output
         return first;
     }
 
     let mut swapped = false;
 
-    for iter_num in 0..iterations {
+    for iter_num in 1..=iterations {
         if swapped {
-            blend_into(square_size, &second, &mut first);
+            blend_into(size, &second, &mut first);
             swapped = false;
         } else {
-            blend_into(square_size, &first, &mut second);
+            blend_into(size, &first, &mut second);
             swapped = true;
         }
         println!("Completed iteration {}/{}", iter_num, iterations);
@@ -78,4 +112,3 @@ pub fn random_blending(size: usize, iterations: usize, square_size: usize) -> Ve
         first
     }
 }
-
