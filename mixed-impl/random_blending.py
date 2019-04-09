1df99f7e94574235af65b820584e6fde1b6c4b01
Alex Addy <alex.addy@gmail.com>
2019-04-09 15:09:17 -0600

    Mixed impl can now switch between debug and release so files

--- mixed-impl/random_blending.py
+++ mixed-impl/random_blending.py
@@ -3,12 +3,13 @@ import argparse
 
 from cffi import FFI
 
+DEBUG_PATH = "./target/debug/librandom_blending_ffi.so"
+RELEASE_PATH = "./target/release/librandom_blending_ffi.so"
+
 ffi = FFI()
 ffi.cdef("""
     int random_blending_c(int, int, int*, char*);
 """)
-
-C = ffi.dlopen("./target/debug/librandom_blending_ffi.so")
 
 PROBABILITIES = [
     1, 8, 8,
@@ -21,12 +22,17 @@ probabilities = ffi.new('int[9]')
 for i in PROBABILITIES:
     probabilities[i] = ffi.cast("int", PROBABILITIES[i])
 
-
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('size', type=int, help="how big the image will be")
     parser.add_argument('iterations', type=int, help='how many iterations to execute')
+    parser.add_argument('--release', help="use release .so", action='store_true')
     args = parser.parse_args()
+
+    if args.release:
+        C = ffi.dlopen(RELEASE_PATH)
+    else:
+        C = ffi.dlopen(DEBUG_PATH)
 
     out_path = ffi.new("char[]", "output.png".encode())
     ret = C.random_blending_c(args.size, args.iterations, PROBABILITIES, out_path)
